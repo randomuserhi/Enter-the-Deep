b8f4d73d3a0bb404eb2a4a0169c643f2ed456e38
randomuserhi <40913834+randomuserhi@users.noreply.github.com>
2021-09-22 19:16:09 +0100

    Added NULL error case

--- Enter the Deep Server/Deep-DynArray.c
+++ Enter the Deep Server/Deep-DynArray.c
@@ -95,6 +95,7 @@ void $Deep_DynArray_Free($Deep_DynArray* arr)
 {
 	free(arr->data);
 	arr->data = NULL;
+	arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
 }
 
 inline void $Deep_DynArray_ErrorFree($Deep_DynArray* arr)
@@ -108,74 +109,92 @@ inline void $Deep_DynArray_ErrorFree($Deep_DynArray* arr)
 
 void $Deep_DynArray_EmptyPush($Deep_DynArray* arr)
 {
-	if (arr->size == arr->capacity)
-	{ 
-		size_t newCapacity = (size_t)(arr->capacity * DEEP_DYNAMIC_ARR_GROWTHRATE);
-		if (newCapacity == arr->capacity) ++newCapacity;
-		void* tmp = realloc(arr->data, arr->typeSize * newCapacity);
-		if (tmp) 
-		{ 
-			arr->data = tmp;
-			arr->capacity = newCapacity;
+	if (arr->data)
+	{
+		if (arr->size == arr->capacity)
+		{
+			size_t newCapacity = (size_t)(arr->capacity * DEEP_DYNAMIC_ARR_GROWTHRATE);
+			if (newCapacity == arr->capacity) ++newCapacity;
+			void* tmp = realloc(arr->data, arr->typeSize * newCapacity);
+			if (tmp)
+			{
+				arr->data = tmp;
+				arr->capacity = newCapacity;
+				++arr->size;
+				arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
+			}
+			else
+			{
+				$Deep_DynArray_ErrorFree(arr);
+				arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_REALLOC;
+			}
+		}
+		else
+		{
 			++arr->size;
 			arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
-		} 
-		else 
-		{ 
-			$Deep_DynArray_ErrorFree(arr);
-			arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_REALLOC;
 		}
-	} 
-	else 
-	{ 
-		++arr->size;
-		arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
-	} 
+	}
+	else arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_DATA_NULL;
 }
 
 void $Deep_DynArray_Push($Deep_DynArray* arr, void* value)
 {
-	$Deep_DynArray_EmptyPush(arr);
+	$Deep_DynArray_EmptyPush(arr); // Any error codes are coughed out by EmptyPush
 	if (arr->data) memcpy(arr->data + (arr->size - 1) * arr->typeSize, value, arr->typeSize);
 }
 
 void $Deep_DynArray_Pop($Deep_DynArray* arr)
 {
-	if (arr->size > 0 && arr->data)
+	if (arr->data)
 	{
-		--arr->size;
+		if (arr->size > 0)
+		{
+			--arr->size;
+			arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
+		}
 	}
+	else arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_DATA_NULL;
 }
 
 void $Deep_DynArray_RemoveAt($Deep_DynArray* arr, size_t index)
 {
-	if (arr->size > 1 && arr->data && index > 0 && index < arr->size)
+	if (arr->data)
 	{
-		size_t size = arr->size - 1 - index;
-		if (size != 0) memmove(arr->data + index * arr->typeSize, arr->data + (index + 1) * arr->typeSize, size * arr->typeSize);
-		--arr->size;
+		if (arr->size > 1 && index > 0 && index < arr->size)
+		{
+			size_t size = arr->size - 1 - index;
+			if (size != 0) memmove(arr->data + index * arr->typeSize, arr->data + (index + 1) * arr->typeSize, size * arr->typeSize);
+			--arr->size;
+			arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
+		}
 	}
+	else arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_DATA_NULL;
 }
 
 void $Deep_DynArray_Shrink($Deep_DynArray* arr)
 {
-	size_t newCapacity = arr->size;
-	if (newCapacity != arr->capacity)
+	if (arr->data)
 	{
-		void* tmp = realloc(arr->data, arr->typeSize * newCapacity);
-		if (tmp)
+		size_t newCapacity = arr->size;
+		if (newCapacity != arr->capacity)
 		{
-			arr->data = tmp;
-			arr->capacity = newCapacity;
-			++arr->size;
-			arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
-		}
-		else
-		{
-			$Deep_DynArray_ErrorFree(arr);
-			arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_REALLOC;
+			void* tmp = realloc(arr->data, arr->typeSize * newCapacity);
+			if (tmp)
+			{
+				arr->data = tmp;
+				arr->capacity = newCapacity;
+				++arr->size;
+				arr->errorCode = DEEP_DYNAMIC_ARR_NO_ERROR;
+			}
+			else
+			{
+				$Deep_DynArray_ErrorFree(arr);
+				arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_REALLOC;
+			}
 		}
 	}
+	else  arr->errorCode = DEEP_DYNAMIC_ARR_ERROR_DATA_NULL;
 }
 
 #endif
