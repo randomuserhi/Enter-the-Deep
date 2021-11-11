6e5c2304dbb6966287e513ccbd4f454847cd36f7
randomuserhi <40913834+randomuserhi@users.noreply.github.com>
2021-11-11 14:08:25 +0000

    Removed asm files

--- Enter the Deep Server/Deep_Engine.s
+++
@@ -1,8 +0,0 @@
-	.file	"Deep_Engine.c"
- # GNU C11 (Rev1, Built by MSYS2 project) version 11.2.0 (x86_64-w64-mingw32)
- #	compiled by GNU C version 11.2.0, GMP version 6.2.1, MPFR version 4.1.0-p13, MPC version 1.2.1, isl version isl-0.24-GMP
-
- # GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
- # options passed: -mtune=generic -march=x86-64 -O3 -std=c11
-	.text
-	.ident	"GCC: (Rev1, Built by MSYS2 project) 11.2.0"
