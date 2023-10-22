209051da3285c7921cd4a0d9317131db1b1d15c8
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-10-22 05:49:40 +0100

    Updated docs

---
+++ Docs/src/main/docs/1.0.0/RHU.ts
@@ -0,0 +1,21 @@
+RHU.require(new Error(), { 
+    docs: "docs", rhuDocuscript: "docuscript",
+}, function({
+    docs, rhuDocuscript,
+}) {
+    const version = "1.0.0";
+    const path = "Random HTML Utility";
+    
+    const page = docuscript<RHUDocuscript.Language, RHUDocuscript.FuncMap>(({
+        h, p, div, br
+    }) => {
+        div(
+            "Random HTML Utility (RHU) is a pure JS library, with TypeScript support, for developing webpages.",
+            br(),
+            "RHU was not made as a competitive replacement to well-known frameworks such as React or Svelt. It instead was made for the learning experience and to experiment with for personal use."
+        );
+        h(1, "About");
+    }, rhuDocuscript);
+    docs.get(version)!.setCache(path, page);
+    return page;
+});
