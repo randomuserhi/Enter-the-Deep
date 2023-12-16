616262e38a7d53aed664b66f2b43c3bbd3461264
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-12-16 17:19:11 +0000

    feat: Altered player loop

---
+++ BackEnd/Assets/Deep/Core/Core.cs
@@ -0,0 +1,118 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+namespace Deep.Core3D {
+    public class Core : MonoBehaviour {
+        private static Core instance;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void RuntimeInitialize() {
+            // TODO(randomuserhi): Check if player loop is maintained between scenes
+            // -> this code may need to be moved to OnSceneLoad
+
+            // Setup Player Loop => Move FixedUpdate.ScriptRunBehaviourFixedUpdate to after FixedUpdate.DirectorFixedUpdatePostPhysics
+            // This is needed because I want what is seen visually to match the current physics step:
+            // OLD:
+            // -> Update velocities, colliders etc...
+            // -> Simulate Step
+            // -> Render -> Say you updated colliders, the colliders on this render may be incorrect post the simulate step
+            // NEW:
+            // -> Simulate Step
+            // -> Update velocities, colliders etc...
+            // -> Render -> colliders are now correct since they will not get mis-aligned due to a simulate step inbetween
+            //
+            // reference: https://medium.com/@thebeardphantom/unity-2018-and-playerloop-5c46a12a677
+
+            PlayerLoopSystem playerLoop = PlayerLoop.GetDefaultPlayerLoop();
+            PlayerLoopSystem fixedUpdate = FindSubSystem<FixedUpdate>(playerLoop);
+            PlayerLoopSystem? scriptRunBehaviourFixedUpdate = null;
+            int i = 0;
+            for (; i < fixedUpdate.subSystemList.Length; ++i) {
+                if (fixedUpdate.subSystemList[i].type == typeof(FixedUpdate.ScriptRunBehaviourFixedUpdate)) {
+                    scriptRunBehaviourFixedUpdate = fixedUpdate.subSystemList[i];
+                    break;
+                }
+            }
+            if (scriptRunBehaviourFixedUpdate == null) {
+                Debug.LogError("Unable to find 'FixedUpdate.ScriptRunBehaviourFixedUpdate' in player loop!");
+            } else {
+                bool found = false;
+                for (; i < fixedUpdate.subSystemList.Length - 1; ++i) {
+                    fixedUpdate.subSystemList[i] = fixedUpdate.subSystemList[i + 1];
+                    if (fixedUpdate.subSystemList[i].type == typeof(FixedUpdate.DirectorFixedUpdatePostPhysics)) {
+                        found = true;
+                        ++i;
+                        break;
+                    }
+                }
+                if (!found) {
+                    Debug.LogWarning("Unable to find 'FixedUpdate.DirectorFixedUpdatePostPhysics' in player loop, appending 'FixedUpdate.ScriptRunBehaviourFixedUpdate' to end.");
+                }
+                fixedUpdate.subSystemList[i] = scriptRunBehaviourFixedUpdate.Value;
+                PlayerLoop.SetPlayerLoop(playerLoop);
+                DebugPlayerLoop(playerLoop);
+            }
+
+            SceneManager.sceneLoaded += OnSceneLoad;
+        }
+
+        private static PlayerLoopSystem FindSubSystem<T>(PlayerLoopSystem def) {
+            if (def.type == typeof(T)) {
+                return def;
+            }
+            if (def.subSystemList != null) {
+                foreach (var s in def.subSystemList) {
+                    var system = FindSubSystem<T>(s);
+                    if (system.type == typeof(T)) {
+                        return system;
+                    }
+                }
+            }
+            return default;
+        }
+
+        private static void DebugPlayerLoop(PlayerLoopSystem playerLoop) {
+            StringBuilder sb = new StringBuilder();
+            _DebugPlayerLoop(playerLoop, sb, 0);
+            Debug.Log(sb.ToString());
+        }
+
+        private static void _DebugPlayerLoop(PlayerLoopSystem playerLoop, StringBuilder sb, int depth) {
+            if (depth == 0) {
+                sb.AppendLine("ROOT NODE");
+            } else if (playerLoop.type != null) {
+                for (int i = 0; i < depth; i++) {
+                    sb.Append("\t");
+                }
+                sb.AppendLine(playerLoop.type.Name);
+            }
+            if (playerLoop.subSystemList != null) {
+                depth++;
+                foreach (var s in playerLoop.subSystemList) {
+                    _DebugPlayerLoop(s, sb, depth);
+                }
+                depth--;
+            }
+        }
+
+        private static void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+            Core exists = FindAnyObjectByType<Core>();
+            if (exists != null) return;
+
+            GameObject go = new GameObject();
+            go.name = "Core";
+            go.AddComponent<Core>();
+        }
+
+        private void Start() {
+            if (instance != null) {
+                Debug.LogError("Cannot have more than one Deep.Core instance");
+                return;
+            }
+            instance = this;
+        }
+    }
+}
--- BackEnd/Assets/Deep/DeepCharacterController3D.cs
+++ BackEnd/Assets/Deep/DeepCharacterController3D.cs
@@ -1,23 +1,65 @@
+using Deep.Math;
 using UnityEngine;
 
+// TODO(randomuserhi) => Manage flipping of gravity properly
+
 namespace Deep {
+    [RequireComponent(typeof(CapsuleCollider))]
     [RequireComponent(typeof(Rigidbody))]
     public class DeepCharacterController3D : MonoBehaviour {
         private Rigidbody rb;
+        private new CapsuleCollider collider;
 
-        private void Start() {
-            rb = GetComponent<Rigidbody>();
-        }
-
-        private bool grounded = false;
+        // Accessors
         public bool Grounded {
             get => grounded;
         }
+        public bool GroundedTransition {
+            get => groundedTransition;
+        }
+        public bool Airborne {
+            get => airborne;
+            set {
+                airborne = value;
+                grounded = !value;
+                if (value) {
+                    airborneTimer = 0;
+                }
+            }
+        }
+
+        // Settings
+        public Vector3 size = new Vector3(1f, 2f, 1f);
+        public Vector3 gravity = Vector3.down * 10f;
+
+        private LayerMask surfaceLayerMask = Physics.AllLayers;
+
+        private float hoverHeight = 0.25f;
+        private float stickyHeight = 1f;
+        private float maxSlopeCosAngle = 0.5f;
+
+        // TODO(randomuserhi): Refactor hover spring -> notably SecondOrderDynamics class
+        private SecondOrderDynamics hoverSpring = new SecondOrderDynamics(5f, 0.5f, 0f, 0f);
+
+        // State
+        private bool grounded = false;
+        private bool prevGrounded = false;
+        private bool groundedTransition = false;
 
         private bool sticky = false;
+        private bool prevSticky = false;
+        private bool stickyTransition = false;
 
-        private void FixedUpdate() {
+        private bool airborne = true;
+        private float airborneTimer = 0f;
 
+        private Vector3 bottom = Vector3.zero;
+        public Vector3 Bottom { get => bottom; }
+        public Vector3 Center { get => bottom - gravity.normalized * size.y; }
+
+        private void Start() {
+            rb = GetComponent<Rigidbody>();
+            collider = GetComponent<CapsuleCollider>();
         }
     }
 }
