525c3db22194012c032d850bfd8eb925b522e9d7
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-12-16 19:42:20 +0000

    chore: Added Editor

---
+++ BackEnd/Assets/Deep/Editor/DeepCharacterController3DEditor.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace Deep.Editor {
+    [CustomEditor(typeof(DeepCharacterController3D))]
+    public class DeepCharacterController3DEditor : UnityEditor.Editor {
+
+    }
+}
+
+#endif
